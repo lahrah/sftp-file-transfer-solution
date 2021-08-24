c2bb837a76b5d2369f4357e1bdc122fb3691ba23
Omolara Savage <omolara.savage@deimos.co.za>
2021-08-24 03:10:14 +0100

    add readme file

--- src/main/java/com/example/sftpfiletransfersolution/service/JschFileTransferService.java
+++
@@ -1,7 +0,0 @@
-package com.example.sftpfiletransfersolution.service;
-
-public interface JschFileTransferService {
-    boolean uploadFile(String localFilePath, String remoteFilePath);
-
-    boolean downloadFile(String localFilePath, String remoteFilePath);
-}
